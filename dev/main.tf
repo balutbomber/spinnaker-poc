db56b97c1e4d7c2214aa701e1aaf365359aaf894
dhcsbalutbomber <richard.bach@dhcs.ca.gov>
2021-02-24 20:37:31 -0800

    update to 0.0.8 of terraform-spinnaker-module

--- dev/main.tf
+++ dev/main.tf
@@ -1,7 +1,7 @@
 variable "github_oauth_token" {}
 
 module "spinnaker" {
-  source = "github.com/balutbomber/terraform-spinnaker-module.git?ref=0.0.7"
+  source = "github.com/balutbomber/terraform-spinnaker-module.git?ref=0.0.8"
   name = "spinnaker"
   region = "us-east-1"
   stage = "dev"
